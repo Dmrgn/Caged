b845f850ef3f3a740e748f62dbde2a97b351a9ff
VinceJH <64177337+VinceJH@users.noreply.github.com>
2022-05-19 21:53:18 -0400

    Other Screens

---
+++ Credits.java
@@ -0,0 +1,43 @@
+/**
+ * <p>
+ * This class contains code for the Credits Screen
+ * </p>
+ *
+ * <h2>ICS 4U0 with Krasteva, V.</h2>
+ *
+ * @version 1.0
+ * @author Ryan Atlas, Samuel Huang and Daniel Morgan
+ * @since May 19th, 2022
+ * <p>
+ * File was created by Samuel Huang on May 19th, 2022.
+ * Samuel Huang spent 10 minutes on this file.
+ * </p>
+ */
+public class Credits
+{
+   /** This Image variable stores the credits page. */
+   private Image creditsPage;
+   /**
+     * The constructor of the Credits Class.
+     */
+   public Credits()
+   {
+   
+   }
+   /**
+     * This method gets called to display the credits screen
+     *
+     */
+   public void display()
+   {
+   
+   }
+   /**
+     * This method sends the user back to the menu screen
+     *
+     */
+   public void back()
+   {
+   
+   }
+}
---
+++ ExitScreen.java
@@ -0,0 +1,43 @@
+/**
+ * <p>
+ * This class contains code for the Exit Screen
+ * </p>
+ *
+ * <h2>ICS 4U0 with Krasteva, V.</h2>
+ *
+ * @version 1.0
+ * @author Ryan Atlas, Samuel Huang and Daniel Morgan
+ * @since May 19th, 2022
+ * <p>
+ * File was created by Samuel Huang on May 19th, 2022.
+ * Samuel Huang spent 10 minutes on this file.
+ * </p>
+ */
+public class ExitScreen
+{
+   /** This Image variable stores the exit page. */
+   private Image exitPage;
+   /**
+     * The constructor of the ExitScreen Class.
+     */
+   public ExitScreen()
+   {
+   
+   }
+   /**
+     * This method gets called to display the exit screen
+     *
+     */
+   public void display()
+   {
+   
+   }
+   /**
+     * This method makes the user leave the game
+     *
+     */
+   public void leave()
+   {
+   
+   }
+}
