beb0fa03a7bcf399bf6887fc8d56c85930f5fdd6
Silence-973 <105831228+Silence-973@users.noreply.github.com>
2022-05-26 22:17:24 -0400

    Fixed comments

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL40.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.animation.AnimationTimer;
+import javafx.scene.*;
 
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
 /**
  * <p>
  * This file acts as the main driver class that runs the game. This is the only class to have a main method
@@ -15,136 +14,86 @@ import org.lwjgl.opengl.GL;
  *
  * <h2>ICS 4U0 with Krasteva, V.</h2>
  *
- * @version 1.1
+ * @version 2.0
  * @author Ryan Atlas, Samuel Huang and Daniel Morgan
  * @since May 17th, 2022
  * <p>
  * 1 hour was spent by Ryan Atlas on this file on May 17th, 18th and 20th, 2022 making the game loop and adding comments.
  * Daniel Morgan spent 3 hours over may 17th and 18th 2022 adding LWJGL graphical functionality
+ * Ryan Atlas spent 1 hour adding the window functionality for JavaFX and adding the main animation loop
+ * for the Player on May 25th and 26th
  * </p>
  */
-public class Main implements Runnable {
-   /** Whether or not the player has exited the main game loop*/
-   public static boolean exited = false;
+public class Main extends Application {
+    /** Whether or not the player has exited the main game loop*/
+    public static boolean exited = false;
     /** The width of the program window */
     private static final int width = 1280;
     /** The height of the program window */
     private static final int height = 720;
     /** The dimensions of the program window stored as a vector */
     private static final Vector dims = new Vector(width, height);
-
-    /** Instance of the main thread this program is running on */
-    private Thread thread;
     /** Boolean set to false when program should exit */
     private boolean running = false;
-
-    /** Pointer to the memory address storing the current window */
-    private long window;
-
     /** Instance of the player */
     public Player player;
-
-    /** Method run when the program should start on a new thread */
-    public void start() {
-        running = true;
-        thread = new Thread(this, "Game");
-        thread.start();
-    }
-
-    /** Method run when the program should initialize GLFW and OpenGL */
-    private void init() {
-        if (!glfwInit()) {
-            throw new RuntimeException("Could not init GlFW.");
-        }
-
-        window = glfwCreateWindow(width, height, "Game", NULL, NULL);
-        if (window == NULL) {
-            throw new RuntimeException("Failed to create the GLFW window.");
-        }
-
-        glfwSetKeyCallback(window, new Keyboard());
-
-        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2);
-
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(1);
-        glfwShowWindow(window);
-
-        GL.createCapabilities();
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(0.0, width, height, 0.0, -1.0, 1.0);
-        glMatrixMode(GL_MODELVIEW);
-
-        glEnable(GL_TEXTURE_2D);
-
-        try{
-            player = new Player(20f, 20f);
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-    }
-
-    /** Main game loop run on the main thread */
-    public void run() {
-        init();
-        while (running) {
-            update();
-            draw();
-
-            if (glfwWindowShouldClose(window))
-                running = false;
-        }
-    }
-
-    /** Handles updating the game logic for the current frame */
-    private void update() {
-        glfwPollEvents();
-        player.update();
-    }
-
-    /** Handles rendering the current frame */
-    private void draw() {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        player.draw();
-        glfwSwapBuffers(window);
-    }
-
-    /** 
+    /**
      * returns the width of the current window 
      * @return Width of the current window in pixels
      */
     public static int getWidth() {
         return width;
     }
-    /** 
+    /**
      * returns the height of the current window 
      * @return Height of the current window in pixels
      */
     public static int getHeight() {
         return height;
     }
-    /** 
+    /**
      * returns the dimensions of the current window as a Vector 
      * @return Dimensions of the current window as a {@link Vector}
      */
     public static Vector getDims() {
         return dims;
     }
-   /**
-   * Main method that runs at the start of the program
-   * and creates a Game object and calls all of its methods
-   * @param args Not used
-   */
-   public static void main(String[] args) {
-      new Main().start();
-      Game isp = new Game();
-      isp.splashScreen();
-      while(!exited) {
-        //  int choice = isp.mainMenu();
+    /**
+     * Overriden start method inherited from JavaFX's Application class.
+     * This method is used to display the window of the game as well as to
+     * control the size of the window and display scenes on it
+     * @param window The window that the game is displayed on
+     */
+    @Override
+    public void start(Stage window){
+        window.setTitle("Caged Inside the Mind");
+        window.setMinWidth(width);
+        window.setMinHeight(height);
+        window.setResizable(false);
+        player = new Player(0, 500);
+        Scene scene = player.getScene();
+        window.setScene(scene);
+        AnimationTimer at = new AnimationTimer() {
+            @Override
+            public void handle (long l) {
+                player.update();
+                player.draw();
+            }
+        };
+        at.start();
+        window.show();
+    }
+    /**
+     * Main method that runs at the start of the program
+     * and creates a Game object and calls all of its methods
+     * @param args Used to call launch
+     */
+    public static void main(String[] args) {
+        launch(args);
+        // Game isp = new Game();
+//       isp.splashScreen();
+//       while(!exited) {
+//         int choice = isp.mainMenu();
 //          switch(choice) {
 //             case 1:
 //                break;
@@ -156,6 +105,6 @@ public class Main implements Runnable {
 //                exited = true;
 //                break;
 //          }
-      }
-   }
+//       }
+    }
 }
--- Player.java
+++ Player.java
@@ -1,60 +1,172 @@
-import java.io.IOException;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import java.util.Timer;
+import java.util.TimerTask;
 /**
  * <p>
  * The player class contains state and behaviours for a player object.
- * The player object can be controled by the keyboard to perform actions such as 
+ * The player object can be controled by the keyboard to perform actions such as
  * movement and slashing. Contains functionality for drawing the player.
  * </p>
  *
  * <h2>ICS 4U0 with Krasteva, V.</h2>
  *
- * @version 1.1
+ * @version 2.0
  * @author Ryan Atlas, Samuel Huang and Daniel Morgan
  * @since May 17th, 2022
  * <p>
  * File was created by Daniel Morgan on May 17th, 2022.
  * Daniel Morgan spent 30 minutes on this file on May 17th
  * Ryan Atlas spent 20 minutes on this file on May 20th, adding new comments and attributes
+ * Ryan Atlas spent 2 hours on this file on May 25th and 26th adding comments, attributes
+ * keyboard input and movement controls for JavaFX
  * </p>
  */
 
 public class Player implements GameObject{
-   /** The player's position stored as a Vector*/
+    /** The player's position stored as a Vector*/
     public Vector pos;
     /** The player's current hp*/
     private int hp;
-    /* The player's movement speed which is a constant*/
+    /** The player's movement speed which is a constant*/
     private static final int SPEED = 10;
-    /** The player's texture sprite */
-    public Texture texture;
+    /** The player's jump height which is a constant*/
+    private static final int JUMP_HEIGHT = 50;
+    /** The player's Sprite as an Image*/
+    public Image sprite;
+    /** The Node that is added to the scene and whose movement is updated*/
+    public Node player;
+    /** Whether the player is jumping or not*/
+    private boolean jump;
+    /** Whether the player is moving left or not*/
+    private boolean left;
+    /** Whether the player is moving right or not*/
+    private boolean right;
+    /** Whether the player is dashing or not*/
+    private boolean dash;
+    /** Whether the player is interacting with an object or not*/
+    private boolean interact;
+    /** Whether the player is grounded or not*/
+    private boolean grounded;
+    /** Current scene*/
+    private Scene scene;
     /**
-    * The Player class constructor which takes in two floats 
-    * in order to initialize the position vector and sets all other
-    * variables to their default.
-    * @param x The player's x coordinate
-    * @param y The player's y coordinate
-    */
-    public Player(float x, float y) throws IOException, Exception {
+     * The Player class constructor which takes in two floats
+     * in order to initialize the position vector and sets all other
+     * variables to their default. The constructor also initializes
+     * the scene and EventHandlers for KeyEvents
+     * @param x The player's x coordinate
+     * @param y The player's y coordinate
+     */
+    public Player(float x, float y) {
         this.pos = new Vector(x, y);
-        texture = new Texture("face.png");
         hp = 100;
-    }
-    /** 
-    * Updates the player's position Vector, implementation of the method
-    * from the GameObject interface
-    */
-    public void update() {
-        Vector diff = Vector.sub(Main.getDims(), pos);
-        float percent = (diff.x + diff.y) / (Main.getWidth() + Main.getHeight());
-        pos = pos.lerp(Main.getDims(), (1 - percent) * 0.01f);
+        jump = false;
+        dash = false;
+        interact = false;
+        grounded = true;
+        right = false;
+        left = false;
+        sprite = new Image("Sprite.png");
+        player = new ImageView(sprite);
+        Group p = new Group(player);
+        scene = new Scene(p, 1280, 720, Color.WHITE);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                if (e.getCode() == KeyCode.D){
+                    right = true;
+                }
+                if (e.getCode() == KeyCode.A) {
+                    left = true;
+                }
+                if (e.getCode() == KeyCode.W) {
+                    jump = true;
+                }
+                if (e.getCode() == KeyCode.SHIFT) {
+                    dash = true;
+                }
+                if (e.getCode() == KeyCode.E){
+                    interact = true;
+                }
+            }
+        });
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                if (e.getCode() == KeyCode.D){
+                    right = false;
+                }
+                if (e.getCode() == KeyCode.A) {
+                    left = false;
+                }
+                if (e.getCode() == KeyCode.W) {
+                    jump = false;
+                }
+                if (e.getCode() == KeyCode.SHIFT) {
+                    dash = false;
+                }
+                if (e.getCode() == KeyCode.E){
+                    interact = false;
+                }
+            }
+        });
     }
     /**
-    * D\aws the player on the screen, implementation of the method
-    * from the GameObject interface
-    */
+     * Updates the player's position Vector based on keyboard input, implementation
+     * of the method from the GameObject interface
+     */
+    public void update() {
+        int dx = 0, dy = 0;
+        if (right) {
+            dx += SPEED/5;
+        } else if (left) {
+            dx -= SPEED/5;
+        }
+        if (dash) {
+            dx *= 2;
+        }
+        pos.x += dx;
+        if (!grounded) {
+            dy = -1*JUMP_HEIGHT;
+            grounded = true;
+        }
+        if (jump && grounded) {
+            grounded = false;
+            dy += JUMP_HEIGHT;
+            Timer jumpTimer = new Timer();
+            TimerTask endJump = new TimerTask() {
+                @Override
+                public void run() {
+                    jump = false;
+                }
+            };
+            jumpTimer.schedule(endJump, 700L);
+        }
+        pos.y -= dy;
+    }
+    /**
+     * Draws the player on the screen, implementation of the method
+     * from the GameObject interface
+     */
     public void draw() {
-        texture.draw(pos);
-        Graphics.drawRect(pos.x, pos.y, 100f, 100f);
+        player.relocate(pos.x, pos.y);
+    }
+    /**
+     * Returns the scene with the player on it so it may be used
+     * in the Main class
+     * @return The current scene
+     */
+    public Scene getScene() {
+        return scene;
     }
 }
