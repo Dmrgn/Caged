376d8eecbcad49a19d1799b6a0a1ca7886423ab3
Ryan Atlas <ryan.atlas973@gmail.com>
2022-06-07 22:21:13 -0400

    Fixed lag

--- src/Game.java
+++ src/Game.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.scene.*;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
@@ -198,6 +198,7 @@ public class Game {
                 player = attachObject(new Player(250, Main.getHeight() - 200), SceneLayer.FOREGROUND);
                 window.setScene(scene);
             } else if (level.levelScreen == 0 && player.pos.y >= 1500) {
+                updateLevelScreen(level, 0);
                 createLevel(level);
                 player = attachObject(new Player(250, Main.getHeight() - 200), SceneLayer.FOREGROUND);
                 window.setScene(scene);
@@ -208,6 +209,7 @@ public class Game {
                 player = attachObject(new Player(1180, 250), SceneLayer.FOREGROUND);
                 window.setScene(scene);
             } else if (level.levelScreen == 1 && player.pos.y >= 1500) {
+                updateLevelScreen(level, 1);
                 createLevel(level);
                 player = attachObject(new Player(250, Main.getHeight() - 200), SceneLayer.FOREGROUND);
                 window.setScene(scene);
