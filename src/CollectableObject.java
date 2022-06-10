94d2acd242077eb87986287d0edbc164554d3905
Ryan Atlas <ryan.atlas973@gmail.com>
2022-06-09 22:18:48 -0400

    Added some comments

--- src/CollectableObject.java
+++ src/CollectableObject.java
@@ -10,34 +10,43 @@ import javafx.scene.input.KeyCode;
  *
  * <h2>ICS 4U0 with Krasteva, V.</h2>
  *
- * @version 3.0
+ * @version 4.0
  * @author Ryan Atlas, Samuel Huang and Daniel Morgan
  * @since June 9th, 2022
  * <p>
  * File was created by Samuel Huang on June 9th, 2022. 30 minutes were spent
+ * Ryan Atlas spent 20 minutes on June 9th adjusted the update() method
  * </p>
  */
-public class CollectableObject extends GameObject implements Interactable
-{
-
+public class CollectableObject extends GameObject implements Interactable {
+    /** Object's node*/
     private Node node;
-    private boolean appear;
+    /** Object's image when not in range*/
     private Image objectOut;
-
+    /** Object's image in range*/
     private Image objectIn;
+    /** Whether the object should be highlighted*/
     private boolean highlighted;
 
-
-    public CollectableObject(Image objectOutRange, Image objectInRange, int x, int y)
-    {
+    /**
+     * Initializes variables and stores the position in the GameObject's
+     * pos vector
+     * @param objectOutRange Image
+     * @param objectInRange Image
+     * @param x X pos
+     * @param y Y pos
+     */
+    public CollectableObject(Image objectOutRange, Image objectInRange, int x, int y) {
         objectOut = objectOutRange;
         objectIn = objectInRange;
         pos = new Vector(x, y);
         node = new ImageView(objectOut);
         highlighted = false;
-        appear = true;
     }
 
+    /**
+     * Updates the item every frame
+     */
     public void update() {
         if (!Game.objectFound) {
             if (!highlighted && inRange((Player) Game.player)) {
@@ -51,21 +60,30 @@ public class CollectableObject extends GameObject implements Interactable
                 //make a boolean true signalling that the object is found.
                 node.setVisible(false);
                 Game.objectFound = true;
-                appear = false;
             }
         } else {
             node.setVisible(false);
         }
     }
-
+    /**
+     * Getter for node
+     * @return The node
+     */
     public Node getNode() {
         return node;
     }
 
+    /**
+     * Checks whether the player is in range to pick up the object
+     * @param p The player
+     * @return Whether they are in range
+     */
     public boolean inRange(Player p) {
         return (Math.abs(p.getNode().getLayoutX() - node.getLayoutX()) < 75 && Math.abs(p.getNode().getLayoutY() - node.getLayoutY()) < 75);
     }
-
+    /**
+     * Display method
+     */
     public void display() {
 
     }
